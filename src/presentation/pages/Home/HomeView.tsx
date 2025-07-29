d47801822b9a93a3e28276088e7b8135dc3af38a
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2025-07-29 11:29:49 -0500

    Vista home (falta el anuncio)

--- src/presentation/pages/Home/HomeView.tsx
+++ src/presentation/pages/Home/HomeView.tsx
@@ -4,29 +4,34 @@ import Logo_transparente from '../../../assets/img/Logo_transparente.png';
 import Borde_home from '../../../assets/img/Borde_home.png';
 import Flecha_izquierda from '../../../assets/img/Flecha_izquierda.png';
 import Flecha_derecha from '../../../assets/img/Flecha_derecha.png'
-import Fondo_pago from '../../../assets/img/Fondo_pago.png'
+import Fond_pago from '../../../assets/img/Fond_pago.jpeg'
 import Laptop_home from '../../../assets/img/Laptop_home.png'
+import Logo_mercado from '../../../assets/img/Logo_mercado.jpeg'
+import { Footer } from '../../layout/components/Footer';
 
 const HomeView: React.FC = () => {
     return (
         <>
             <div className=' w-full h-[10vh] rounded-b-full'>
-                <img className='h-[25vh] w-full' src={Borde_home}/>
+                <img className='h-[15vh] w-full' src={Borde_home}/>
             </div>
 
-                <div className='flex justify-center items-center px-10 gap-[10%] mt-[20vh]'>
-                    <div className=' flex items-center justify-center rounded-lg'>
-                        <img src={Logo_transparente} alt='Logo' className='w-[30vw]  max-h-full max-w-full object-contain' />
+                <div className='flex justify-center items-center px-10 gap-[6%] mt-[22vh]'>
+                  <div className='w-[40vw] h-[40vh] flex items-center justify-center rounded-lg'>
+                    <img
+                        src={Logo_transparente}
+                        alt='Logo'
+                        className='w-full h-[60vh] object-contain'
+                    />
                     </div>
-                    <div className='w-[20vw] h-[30vh] flex items-center justify-center rounded-lg'>
+                        <div className='flex items-center justify-center rounded-lg'>
                         <img
-                        className='max-h-full max-w-full object-contain'
-                        src={Laptop_home}
-                        alt='Promoción'
+                            className='w-[40vw] h-[50vh] object-contain'
+                            src={Laptop_home}
+                            alt='Promoción'
                         />
-                    </div>
+                        </div>
                 </div>
-
             <div className='flex justify-center my-4'>
                 <button className='bg-[#14489D] w-[12vw] text-white rounded-md py-2 shadow-md hover:bg-[#123c80] transition'>
                     Ir a tienda
@@ -56,10 +61,42 @@ const HomeView: React.FC = () => {
                     <img className='w-[2vw] h-[2vw] cursor-pointer' src={Flecha_derecha} alt='Flecha derecha' />
                 </div>
             </div>
+                <div className='relative w-full h-[80vh]'>
+                        <img
+                            className='w-full h-full '
+                            src={Fond_pago}
+                            alt='Fondo de pago'
+                        />
 
-            <div>
-                <img className='w-full' src={Fondo_pago}/>
-            </div>
+                <div className='absolute top-1/2 right-12 transform -translate-y-1/  max-w-[40vw] bg-opacity-50 p-6 '>
+                       <div className='text-3xl text-[#14489D] font-bold mb-4 flex flex-wrap items-center'>
+                            <span>Paga fácil y seguro </span>
+                            <div className='flex items-center '>
+                                <span>con Mercado Pago</span>
+                                <img
+                                className='h-[5vh] w-auto ml-2'
+                                src={Logo_mercado}
+                                alt='Logo Mercado Pago'
+                                />
+                            </div>
+                        </div>
+
+                        <p className='mb-2 text-[#484848]'>
+                             Elige el método que más se ajuste a ti:
+                        </p>
+                    <ul className='list-disc text-[#484848] list-inside mb-4'>
+                        <li>Tarjeta de crédito</li>
+                        <li>Tarjeta de débito</li>
+                    </ul>
+                        <p className='text-[#14479D] font-semibold'>
+                            Tu pago se acredita al instante para procesar 
+                        </p>
+                        <span  className='text-[#14479D] font-semibold'>tu pedido sin demoras.</span>
+                </div>
+                <Footer/>
+                </div>
+
+
 
         </>
     );
