b9eeada98f13ffd837fc035325466378fdaebfe3
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2025-07-28 22:29:22 -0500

    Cards

---
+++ src/presentation/layout/components/CardDetails.tsx
@@ -0,0 +1,53 @@
+import Compu from '../../../assets/img/Compu.png';
+
+export const CardDetail = () => {
+    return (
+        <div>
+        <div className="flex bg-white  rounded-xl  w-[80vw] py-[5vh] max-w-5xl h-[55vh] mx-auto">
+        
+            <div className=" bg-white border border-slate-100 rounded-2xl shadow-2xl flex items-center justify-center w-[30vw] ">
+                <img
+                    src={Compu}
+                    alt="Laptop"
+                    className="w-[80%] h-auto object-contain"
+                />
+            </div>
+
+            <div className="text-blue-600 px-[5vw] w-[30vw] flex flex-col justify-between">
+                <div>
+                    <h2 className="text-xl font-bold leading-snug">
+                        ASUS <br />
+                        Laptop VivoBook Go 15 E1504F 15.6 pulgadas
+                    </h2>
+                    <p className="text-lg font-semibold mt-4">$20,000 mxn</p>
+                </div>
+
+                <div className="mt-6">
+                    <label className="block mb-1 text-sm font-medium">Cantidad</label>
+                    <div className="flex items-center gap-4">
+                        <input
+                            type="number"
+                            min="1"
+                            defaultValue="1"
+                            className="w-[5rem] h-[10vh] p-2 rounded-md text-black border border-gray-300"
+                        />
+                      <div className='flex flex-col'>
+                         <div>
+                             <button className="bg-white border-slate-300 border text-[#14489D] px-4 py-2 rounded-full w-[27vw] font-semibold hover:bg-gray-100 transition">
+                                Agregar al carrito
+                            </button>
+                       </div>
+                       <div>
+                            <button className="border border-slate-300 mt-4 text-blue-500 px-4 py-2 rounded-full w-[27vw] font-semibold hover:bg-white hover:text-[#14489D] transition">
+                                Comprar ahora
+                            </button>
+                       </div>
+                      </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
--- src/presentation/pages/Home/HomeView.tsx
+++ src/presentation/pages/Home/HomeView.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { CardsProduct } from '../../layout/components/CardProduct';
 
 const HomeView: React.FC = () => {
-    return(
+    return (
         <>
-            Hola
+            <div className='bg-[#14489D] w-full h-[20vh] rounded-b-full'></div>
+
+            <div className='flex justify-between mt-4 px-10'>
+                <div className='bg-slate-200 w-[20vw] h-[30vh] flex items-center justify-center rounded-lg shadow-md'>
+                    Imagen
+                </div>
+                <div>
+                    <img
+                        className='w-[20vw] rounded-lg shadow-md'
+                        src='https://i5-mx.walmartimages.com/mg/gm/1p/images/product-images/img_large/00075976321504-1l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF'
+                        alt='Promoción'
+                    />
+                </div>
+            </div>
+
+            <div className='flex justify-center my-4'>
+                <button className='bg-[#14489D] w-[12vw] text-white rounded-md py-2 shadow-md hover:bg-[#123c80] transition'>
+                    Ir a tienda
+                </button>
+            </div>
+
+            <div className='bg-[#14479D] w-full h-[10vh] mt-[2vh] flex items-center justify-center'>
+                <p className='text-white font-semibold text-2xl'>
+                    Conoce todas nuestras promociones
+                </p>
+            </div>
+
+            <div className='w-full my-[8vh] px-8'>
+                <div className='flex '>
+                    {[...Array(4)].map((_, i) => (
+                        <div key={i} className='min-w-[250px] max-w-[250px]'>
+                            <CardsProduct />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
-)
-}
+    );
+};
 
 export default HomeView;
