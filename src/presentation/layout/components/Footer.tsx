a2a1e79c314e553c5e70948cbc6c8026fd35da50
Andrea <132911113+Goraz23@users.noreply.github.com>
2025-07-30 07:34:52 -0500

    Se sube home

--- src/presentation/layout/components/Footer.tsx
+++ src/presentation/layout/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="bg-gray-100 text-center text-sm text-gray-600 py-4 ">
+        <footer className="bg-[#14489D] text-center text-sm text-white py-4 ">
             © {new Date().getFullYear()} MiTienda. Todos los derechos reservados.
         </footer>
     );
--- src/presentation/pages/Home/HomeView.tsx
+++ src/presentation/pages/Home/HomeView.tsx
@@ -10,12 +10,13 @@ import { Footer } from '../../layout/components/Footer';
 import { laptopsServerApi } from '../../../infrastructure/http/features/laptopsServerApi';
 import React, { useEffect, useState, useRef } from 'react';
 import type { LaptopDto } from '../../../contracts/laptop/laptopDto';
-import {CardsProduct} from "../../components/CardProduct.tsx";
+import { CardsProduct } from "../../components/CardProduct.tsx";
+import image10 from '@/assets/img/image10.png';
 
 const HomeView: React.FC = () => {
     const [data, setData] = useState<LaptopDto[]>()
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         try {
             const response = await laptopsServerApi.getAll()
             setData(response)
@@ -29,11 +30,11 @@ const HomeView: React.FC = () => {
     }, [])
     const scrollRef = useRef<HTMLDivElement>(null);
 
-        const scrollRight = () => {
-            if (scrollRef.current) {
-                scrollRef.current.scrollBy({ left: 300, behavior: 'smooth' });
-            }
-        };
+    const scrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({ left: 300, behavior: 'smooth' });
+        }
+    };
 
 
 
@@ -42,25 +43,25 @@ const HomeView: React.FC = () => {
 
         <>
             <div className=' h-[10vh] md:h-[20vh] rounded-b-full gap-0'>
-                <img className='h-full w-full' src={Borde_home}/>
+                <img className='h-full w-full' src={Borde_home} />
             </div>
 
-               <div className='flex flex-col md:flex-row justify-center items-center px-6 md:px-10 gap-8 md:gap-[6%]'>
-                    <div className='w-full md:w-[40vw] h-[45%] md:h-[40%] flex items-center justify-center rounded-lg'>
-                        <img
+            <div className='flex flex-col md:flex-row justify-center items-center px-6 md:px-10 gap-8 md:gap-[6%]'>
+                <div className='w-full md:w-[40vw] h-[45%] md:h-[40%] flex items-center justify-center rounded-lg'>
+                    <img
                         src={Logo_transparente}
                         alt='Logo'
                         className='w-[50%] md:w-auto pt-4 md:pt-0 max-h-full object-contain'
-                        />
-                    </div>
-                    <div className='w-full md:w-[40vw] h-[35vh] md:h-[50vh] flex items-center justify-center rounded-lg float-smooth'>
-                        <img
+                    />
+                </div>
+                <div className='w-full md:w-[40vw] h-[35vh] md:h-[50vh] flex items-center justify-center rounded-lg float-smooth'>
+                    <img
                         src={Laptop_home}
                         alt='Promoción'
                         className='w-auto max-h-full object-contain'
-                        />
-                    </div>
+                    />
                 </div>
+            </div>
 
             <div className='flex justify-center my-4 pb-[5%]'>
                 <button className='bg-[#14489D] w-[25vw] md:w-[12vw] text-white  py-3 shadow-md hover:bg-[#123c80] transition rounded-3xl montserrat font-semibold hover:font-bold'>
@@ -69,77 +70,88 @@ const HomeView: React.FC = () => {
             </div>
 
             <div className='bg-[#14479D] w-full h-[10vh] mt-[2vh] flex items-center justify-center mx-auto'>
-                <p className='text-[#E4E9F5] text-center font-semibold text-3xl md:text-5xl montserrat'>
+                <p className='text-[#E4E9F5] text-center font-semibold text-2xl md:text-5xl montserrat'>
                     Conoce todas nuestras promociones
                 </p>
             </div>
-              <div className='w-full h-[80vh] flex flex-col  border-t-slate-400 my-[8vh] px-8 relative'>
-                <div className='mb-4 px-[5vw] mt-[6vh] justify-between flex'>
-                   <div>
-                        <p className='font-semibold text-[#484848] text-2xl mb-1'>Lo más nuevo en Sell IT</p>
-                        <span className='text-[#484848]'>Laptops al mejor precio... ¡no te quedes sin la tuya!</span>
+            <div className='w-full h-[80vh] flex flex-col  border-t-slate-400 my-[8vh] px-8 relative montserrat'>
+                <div className='mb-4 px-[5vw] mt-[3vh] md:mt-[6vh] flex flex-col items-center text-center md:flex-row md:justify-between md:items-start md:text-left gap-4 md:items-center'>
+                    <div>
+                        <p className='font-bold text-[#484848] text-5xl mb-1'>Lo más nuevo en Sell IT</p>
+                        <span className='text-[#484848] font-medium text-xl'>Laptops al mejor precio... ¡no te quedes sin la tuya!</span>
                     </div>
                     <div>
-                        <button className='bg-[#1E4E9C] text-white w-[25vw] md:w-[8vw] h-[4vh] rounded-2xl'>
+                        <button className='bg-[#1E4E9C] text-white w-[25vw] md:w-[8vw] py-3 rounded-3xl'>
                             Ver todo
                         </button>
                     </div>
                 </div>
-                    <div className='flex items-center justify-center flex-1 relative'>
-                        <div className='absolute left-4 top-1/2 transform -translate-y-1/2 z-10'>
-                            <img className='w-[2vw] h-[2vw] cursor-pointer' src={Flecha_izquierda} alt='Flecha izquierda' />
-                        </div>
+                <div className='flex items-center justify-center flex-1 relative mx-auto overflow-hidden w-max-100'>
 
-                        <div ref={scrollRef} className=' overflow-x-auto overflow-y-hidden scrollbar-hide flex gap-12'>
+                    <div className='absolute overflow-hidden left-4 top-1/2 transform -translate-y-1/2 z-10'>
+                        <img className='w-[2vw] h-[2vw] cursor-pointer' src={Flecha_izquierda} alt='Flecha izquierda' />
+                    </div>
+
+                    <div ref={scrollRef} className='overflow-scroll  overflow-y-hidden scrollbar-hide flex gap-12 '>
                         {
                             data && (
                                 data.map((item, index) => (
-                                    <div key={index} className='min-w-[250px] max-w-[250px]'>
-                                    <CardsProduct laptop={item}/>
+                                    <div key={index} className='min-w-[250px] max-w-[250px] w-full md:w-[23%] hidden md:block first:block"'>
+                                        <CardsProduct laptop={item} />
                                     </div>
                                 ))
                             )
                         }
-                        </div>
-                        <div className='absolute right-4 top-1/2 transform -translate-y-1/2 z-10'>
-                            <img className='w-[2vw] h-[2vw] cursor-pointer '  onClick={scrollRight} src={Flecha_derecha} alt='Flecha derecha'  />
-                        </div>
+                    </div>
+                    <div className='absolute right-4 top-1/2 transform -translate-y-1/2 z-10'>
+                        <img className='w-[2vw] h-[2vw] cursor-pointer ' onClick={scrollRight} src={Flecha_derecha} alt='Flecha derecha' />
                     </div>
                 </div>
-                <div className='relative w-full h-[80vh]'>
-            <img
-                className='w-full h-full '
-                src={Fond_pago}
-                alt='Fondo de pago'
-            />
-                <div className='absolute md:text-sm top-1/2 right-0 md:right-12 transform -translate-y-1/2 max-w-full md:max-w-[40vw] min-w-full md:min-w-[5vw] px-4 md:px-0md:rounded-none'
-                >
-                <div className='text-2xl md:text-3xl text-[#14489D] font-bold mb-4'>
-                    <p>Paga fácil y seguro</p>
-                    <div className='flex items-center flex-wrap'>
-                    <span>con Mercado Pago</span>
-                    <img
-                        className='h-[5vh] w-auto ml-2'
-                        src={Logo_mercado}
-                        alt='Logo Mercado Pago'
-                    />
-                    </div>
-                </div>
-                <p className='mb-2 text-[#484848]'>
-                    Elige el método que más se ajuste a ti:
-                </p>
-                <ul className='list-disc text-[#484848] list-inside mb-4'>
-                    <li>Tarjeta de crédito</li>
-                    <li>Tarjeta de débito</li>
-                </ul>
-                <p className='text-[#14479D] font-semibold'>
-                    Tu pago se acredita al instante para procesar tu pedido sin demoras.
-                </p>
-                </div>
-
-            <Footer />
             </div>
+            <div className='relative w-full h-[80vh] montserrat'>
+                <img
+                    className='w-full h-full '
+                    src={Fond_pago}
+                    alt='Fondo de pago'
+                />
+                <div className='absolute md:text-sm top-1/2 flex transform -translate-y-1/2 max-w-full md:max-w-[100vw] min-w-full md:min-w-[5vw] px-4 md:px-0md:rounded-none items-center justify-center w-full gap-24'
+                >
+                    <div className='flex flex-col items-center justify-center w-full md:w-[100vw]'>
+                        <img
+                            className='w-min-[500px] h-auto mb-[-12%] '
+                            src={image10}
+                            alt='Fondo de pago'
+                        />
+                    </div>
+                    <div className='flex flex-col items-center justify-start w-full md:w-[100vw]'>
+                        <div className='text-2xl md:text-3xl text-[#14489D] font-bold mb-4 justify-start'>
+                            <p>Paga fácil y seguro</p>
+                            <div className='flex items-center flex-wrap'>
+                                <span>con Mercado Pago</span>
+                                <img
+                                    className='h-[5vh] w-auto ml-2'
+                                    src={Logo_mercado}
+                                    alt='Logo Mercado Pago'
+                                />
+                            </div>
+                        </div>
+                        <p className='mb-2 text-[#484848]'>
+                            Elige el método que más se ajuste a ti:
+                        </p>
+                        <ul className='list-disc text-[#484848] list-inside mb-4'>
+                            <li>Tarjeta de crédito</li>
+                            <li>Tarjeta de débito</li>
+                        </ul>
+                        <p className='text-[#14479D] font-semibold'>
+                            Tu pago se acredita al instante para procesar tu pedido sin demoras.
+                        </p>
+                    </div>
 
+                </div>
+
+
+            </div>
+            
         </>
     );
 };
